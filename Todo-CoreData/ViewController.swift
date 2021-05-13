151af8f66f0cb08deb5654bd152283f5f30bd5a3
dantmac <dantmac@mail.ru>
2021-05-13 22:49:28 +0300

    initial commit

---
+++ Todo-CoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Todo-CoreData
+//
+//  Created by admin on 13.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
